bef448fecdff1640bd4b41904cf9d7ed5583930f
frostnight <frostnightsong@gmail.com>
2022-11-27 21:03:10 +0900

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
+
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id, "data": "hello fastapi"})

6835b2990b21a3214fcdafa6865030f78f66c543
frostnight <frostnightsong@gmail.com>
2022-12-10 17:47:23 +0900

    fast api mongodb 모델 추가

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
+from app.models import mongodb
+from app.models.book import Book
 
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -10,6 +12,28 @@ app = FastAPI()
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("item.html", {"request": request, "id": id, "data": "hello fastapi"})
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    book = Book(keyword="파이썬", publisher="BJPublic", price=1200, image='me.png')
+    print(await mongodb.engine.save(book))     # DB에 저장
+    return templates.TemplateResponse("./index.html",
+                                      {"request": request,
+                                       "title": "콜렉터 북북이"})
+
+
+@app.get("/search", response_class=HTMLResponse)
+async def search(request: Request, q: str):
+    return templates.TemplateResponse("./index.html",
+                                      {"request": request,
+                                       "title": "콜렉터 북북이",
+                                       "keyword": q})
+
+
+@app.on_event("startup")
+async def on_app_start():
+    mongodb.connect()
+
+
+@app.on_event("shutdown")
+async def on_app_shutdown():
+    mongodb.close()
